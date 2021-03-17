aff3791f5ff00c72ec24ac350a003e785e365dbe
Santanu Karmakar <skarmakar.personal@gmail.com>
2021-03-17 18:29:23 +0530

    Creating a layout component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,12 @@
-import './App.css';
-
 import React, { useState } from 'react'
-import Person from './Person/Person'
+import Layout from './components/Layout/Layout'
 
 // this is a stateful component
 function App() {
-  const [personsState, setPersonState] = useState({
-    persons: [
-      {id: 'asdasd', name: 'Max', age: 20},
-      {id: 'dfdffd', name: 'Manu', age: 20},
-      {id: 'cxvxvx', name: 'Hanu', age: 20}
-    ],
-  });
-
-  const [personVisible, setPersonVisible] = useState({
-    showPersons: false
-  });
-
-  // multuple setState calls can be used to manage multiple states
-
-  // this is a function
-  const deletePersonHandler = (personIndex) => {
-    let persons = [...personsState.persons]; // this is creating a copy and does not modify the original method using a spread
-    persons.splice(personIndex, 1);
-    setPersonState({ persons: persons });
-  }
-
-  const togglePersonsHandler = () => {
-    const doesShow = personVisible.showPersons;
-    setPersonVisible({ showPersons: !doesShow });
-  }
-
-  let persons = null;
-  if(personVisible.showPersons) {
-    persons = (
-      <div>
-        { 
-          personsState.persons.map((person, index) => {
-            return <Person 
-              key={person.id}
-              name={person.name} 
-              age={person.age} 
-              click={() => deletePersonHandler(index)} />
-          })
-        }
-      </div>
-    )
-  }
-
   return (
-    <div className="react-demo-app">
-      <h2>I am learning react!</h2>
-      <button onClick={togglePersonsHandler}>TogglePersons</button><br/>
-      { persons }
-    </div>
+    <Layout>
+      <p>Check children</p>
+    </Layout>
   );
 }
 
