d7d154794db7b273a599afec4c744dbfaf010e28
Santanu Karmakar <skarmakar.personal@gmail.com>
2021-03-18 23:46:08 +0530

    Rendering burger ingredients and using css modules

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
-import './Layout.css';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main className='content'>
+        <main className={classes.content}>
             {props.children}
         </main>
     </Aux>
